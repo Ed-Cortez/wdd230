5f9b0cc42e712896946086ffca56eecaa681e078
Ed-Cortez <dom21001@byui.edu>
2023-05-13 01:22:18 -0500

    Commit

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -7,3 +7,21 @@ function toggleMenu () {
 const x = document.getElementById("hamburgerBtn")
 
 x.onclick = toggleMenu;
+
+
+/* LAST UPDATE DATE */
+
+const date = document.querySelector("#date");
+
+try {
+	const options = {
+		weekday: "short",
+		day: "numeric",
+		month: "short",
+		year: "numeric"
+	};
+	date.innerHTML = `<span class="highlight">${new Date().toLocaleDateString("en-UK", options)}</span>`;
+} catch (e) {
+	alert("Date not displayed, the browser may not support it");
+}
+
